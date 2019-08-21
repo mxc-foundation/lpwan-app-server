d27269037559c8d0f4a97a06c17414a4ac3f3524
Jane <janejia1019@gmail.com>
2019-08-21 14:15:38 +0000

    Revert "Merge branch '25-login-with-default-values-for-demo' into 'develop'"
    This reverts merge request !49

--- ui/src/views/users/Login.js
+++ ui/src/views/users/Login.js
@@ -41,13 +41,7 @@ class LoginForm extends FormComponent {
     const extraButtons = [
       <Button color="primary" component={Link} to={`/registration`} type="button" disabled={false}>Register</Button>
     ]
-    let demoUsername = "";
-    let demoPassword = "";
-    if(window.location.origin.includes("demo.")){
-      demoUsername = process.env.REACT_APP_DEMO_USER;
-      demoPassword = process.env.REACT_APP_DEMO_USER_PASSWORD;
-    }
-    
+
     return(
       <Form
         submitLabel={this.props.submitLabel}
@@ -58,7 +52,7 @@ class LoginForm extends FormComponent {
           id="username"
           label="Username"
           margin="normal"
-          value={this.state.object.username === undefined ? demoUsername : this.state.object.username }
+          value={this.state.object.username || ""}
           onChange={this.onChange}
           fullWidth
           required
@@ -68,8 +62,7 @@ class LoginForm extends FormComponent {
           label="Password"
           type="password"
           margin="normal"
-          value={this.state.object.password === undefined ? demoPassword : this.state.object.password }
-          helperText="build@mxc.org is for demo user. You can access with this account right now."
+          value={this.state.object.password || ""}
           onChange={this.onChange}
           fullWidth
           required
@@ -85,7 +78,7 @@ class Login extends Component {
     super();
 
     this.state = {
-      registration: null
+      registration: null,
     };
 
     this.onSubmit = this.onSubmit.bind(this);
