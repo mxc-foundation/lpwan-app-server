22dba1a768fec1c48183564c3c39305a5c5d1114
NamGyeong <namgyeong.cho@mxc.org>
2019-09-20 16:50:41 +0200

    imprv code

--- ui/src/util/Util.js
+++ ui/src/util/Util.js
@@ -2,11 +2,8 @@ export function getM2MLink() {
     let host = process.env.REACT_APP_M2M_LOCAL_SERVER;
     const origin = window.location.origin;
     
-    if(origin.includes(process.env.REACT_APP_DEMO_HOST_SERVER)){
-        host = process.env.REACT_APP_M2M_DEMO_SERVER;
-    }
-    if(origin.includes(process.env.REACT_APP_TEST_HOST_SERVER)){
-        host = process.env.REACT_APP_M2M_TEST_SERVER;
+    if(origin.includes(process.env.REACT_APP_SUBDOM_LORA)){
+        host = origin.replace(process.env.REACT_APP_SUBDOM_LORA, process.env.REACT_APP_SUBDOM_M2M);
     }
     
     return host;
