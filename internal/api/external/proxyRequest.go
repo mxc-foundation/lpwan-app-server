080c3accf5543cedbbbc706fd65a07aa10daec99
lixuan <lixuan@mxc.org>
2019-10-18 10:53:09 +0200

    implement api: get balance

--- internal/api/external/proxyRequest.go
+++ internal/api/external/proxyRequest.go
@@ -2,8 +2,13 @@ package external
 
 import (
 	pb "github.com/brocaar/lora-app-server/api"
+	m2m_api "github.com/brocaar/lora-app-server/api/m2m_server"
 	"github.com/brocaar/lora-app-server/internal/api/external/auth"
+	"github.com/brocaar/lora-app-server/internal/backend/m2m_client"
+	"github.com/brocaar/lora-app-server/internal/config"
 	"golang.org/x/net/context"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type ProxyRequestAPI struct {
@@ -17,7 +22,16 @@ func NewProxyRequestAPI(validator auth.Validator) *ProxyRequestAPI {
 }
 
 func (a *ProxyRequestAPI) GetWalletBalance(ctx context.Context, req *pb.GetWalletBalanceRequest) (*pb.GetWalletBalanceResponse, error) {
+	m2mClient, err := m2m_client.GetPool().Get(config.C.M2MServer.M2MServer, []byte(config.C.M2MServer.CACert),
+		[]byte(config.C.M2MServer.TLSCert), []byte(config.C.M2MServer.TLSKey))
+	if err != nil {
+		return &pb.GetWalletBalanceResponse{}, status.Errorf(codes.Unavailable, err.Error())
+	}
 
+	resp, err := m2mClient.GetWalletBalance(ctx, &m2m_api.GetWalletBalanceRequest{OrgId: req.OrgId})
+	if err != nil {
+		return &pb.GetWalletBalanceResponse{}, status.Errorf(codes.Unavailable, err.Error())
+	}
 
-	return &pb.GetWalletBalanceResponse{}, nil
+	return &pb.GetWalletBalanceResponse{Balance: resp.Balance}, nil
 }
