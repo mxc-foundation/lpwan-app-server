b8dba0906759f2290d2c8e06cf5d2423b67aab38
CoderShiun <123657@gmail.com>
2019-12-12 17:49:41 +0100

    added wallet api logic

--- internal/api/external/external.go
+++ internal/api/external/external.go
@@ -106,6 +106,7 @@ func setupAPI(conf config.Config) error {
 	m2m_api.RegisterStakingServiceServer(grpcServer, m2m_ui.NewStakingServerAPI(validator))
 	m2m_api.RegisterSuperNodeServiceServer(grpcServer, m2m_ui.NewSupernodeServerAPI(validator))
 	m2m_api.RegisterTopUpServiceServer(grpcServer, m2m_ui.NewTopUpServerAPI(validator))
+	m2m_api.RegisterWalletServiceServer(grpcServer, m2m_ui.NewWalletServerAPI(validator))
 
 	// setup the client http interface variable
 	// we need to start the gRPC service first, as it is used by the
@@ -298,6 +299,9 @@ func getJSONGateway(ctx context.Context) (http.Handler, error) {
 		return nil, errors.Wrap(err, "register proxy request handler error")
 	}
 	if err := m2m_pb.RegisterTopUpServiceHandlerFromEndpoint(ctx, mux, apiEndpoint, grpcDialOpts); err != nil {
+		return nil, errors.Wrap(err, "register proxy request handler error")
+	}
+	if err := m2m_pb.RegisterWalletServiceHandlerFromEndpoint(ctx, mux, apiEndpoint, grpcDialOpts); err != nil {
 		return nil, errors.Wrap(err, "register proxy request handler error")
 	}
 
