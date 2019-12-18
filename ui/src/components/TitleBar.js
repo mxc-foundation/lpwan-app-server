55cda375f9e4f67784486ad06cb2f6c52a5b91b8
Luke Schoen <ltfschoen@gmail.com>
2019-12-18 04:17:28 +0100

    fix: Prevent TitleBar buttons from wrapping

--- ui/src/components/TitleBar.js
+++ ui/src/components/TitleBar.js
@@ -7,11 +7,11 @@ class TitleBar extends Component {
   render() {
     return (
       <Row className="pt-3 pb-2">
-        <Col lg={6}>
+        <Col xs={8}>
           {this.props.children}
         </Col>
 
-        <Col lg={6} className="text-right">
+        <Col xs={4} className="text-right">
           {this.props.buttons}
         </Col>
       </Row>
