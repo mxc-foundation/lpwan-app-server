48073619775e4a612fcab65906577f50c7f816b4
Luke Schoen <ltfschoen@gmail.com>
2019-12-19 08:12:36 +0100

    refactor: Remove disused comment

--- ui/src/views/users/UserProfilePicFile.js
+++ ui/src/views/users/UserProfilePicFile.js
@@ -36,7 +36,7 @@ function UserProfilePicFile(props) {
   const onDropRejected = useCallback((files) => {
     const { onChange } = props;
     let output = {};
-    const errorMessage = i18n.t(`${packageNS}:tr000460`, { maxFileSizeMegabytes: MAX_FILE_SIZE_MEGABYTES, filename: files[0].name });//`Maximum file upload size of ${MAX_FILE_SIZE_MEGABYTES} MB exceeded by file ${files[0].name}`;
+    const errorMessage = i18n.t(`${packageNS}:tr000460`, { maxFileSizeMegabytes: MAX_FILE_SIZE_MEGABYTES, filename: files[0].name });
     console.error(errorMessage);
     output.errorMessage = errorMessage;
     onChange && onChange(output);
