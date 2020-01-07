5154491e2dc06c33835524be00c837edc81a44ae
NamGyeong <namgyeong.cho@mxc.org>
2020-01-07 14:02:24 +0100

    MCL-383 change logo

--- ui/src/views/users/CreateUser.js
+++ ui/src/views/users/CreateUser.js
@@ -33,6 +33,8 @@ const styles = theme => ({
 
 class CreateUser extends Component {
   onSubmit = (user) => {
+    console.log('user', user);
+    return false;
     UserStore.create(user, user.password, [], resp => {
       this.props.history.push("/users");
     });
