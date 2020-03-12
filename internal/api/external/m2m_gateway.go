bed55a26b636526c702d4564ebd229bb2b6001a7
lixuan <lixuan@mxc.org>
2020-03-12 14:51:37 +0100

    MD-882 : fix device, gateway, settings service unavailable problem

--- internal/api/external/m2m_gateway.go
+++ internal/api/external/m2m_gateway.go
@@ -2,8 +2,10 @@ package external
 
 import (
 	"context"
+	"strconv"
 
 	api "github.com/mxc-foundation/lpwan-app-server/api/appserver_serves_ui"
+	m2mServer "github.com/mxc-foundation/lpwan-app-server/api/m2m_serves_appserver"
 	"github.com/mxc-foundation/lpwan-app-server/internal/api/external/auth"
 	"github.com/mxc-foundation/lpwan-app-server/internal/backend/m2m_client"
 	"github.com/mxc-foundation/lpwan-app-server/internal/config"
@@ -26,131 +28,204 @@ func NewGatewayServerAPI(validator auth.Validator) *GatewayServerAPI {
 
 // GetGatewayList defines the get Gateway list request and response
 func (s *GatewayServerAPI) GetGatewayList(ctx context.Context, req *api.GetGatewayListRequest) (*api.GetGatewayListResponse, error) {
-	log.WithField("orgId", req.OrgId).Info("grpc_api/GetGatewayList")
+	logInfo := "api/appserver_serves_ui/GetGatewayList org=" + strconv.FormatInt(req.OrgId, 10)
 
-	prof, err := getUserProfileByJwt(ctx, s.validator, req.OrgId)
+	// verify if user is global admin
+	userIsAdmin, err := s.validator.GetIsAdmin(ctx)
 	if err != nil {
-		return &api.GetGatewayListResponse{}, status.Errorf(codes.Unauthenticated, err.Error())
+		log.WithError(err).Error(logInfo)
+		return &api.GetGatewayListResponse{}, status.Errorf(codes.Internal, "unable to verify user: %s", err.Error())
+	}
+	// is user is not global admin, user must have accesss to this organization
+	if userIsAdmin == false {
+		if err := s.validator.Validate(ctx, auth.ValidateOrganizationAccess(auth.Read, req.OrgId)); err != nil {
+			log.WithError(err).Error(logInfo)
+			return &api.GetGatewayListResponse{}, status.Errorf(codes.Unauthenticated, "authentication failed: %s", err.Error())
+		}
 	}
 
 	m2mClient, err := m2m_client.GetPool().Get(config.C.M2MServer.M2MServer, []byte(config.C.M2MServer.CACert),
 		[]byte(config.C.M2MServer.TLSCert), []byte(config.C.M2MServer.TLSKey))
 	if err != nil {
+		log.WithError(err).Error(logInfo)
 		return &api.GetGatewayListResponse{}, status.Errorf(codes.Unavailable, err.Error())
 	}
 
-	gwClient := api.NewGSGatewayServiceClient(m2mClient)
+	gwClient := m2mServer.NewGSGatewayServiceClient(m2mClient)
 
-	resp, err := gwClient.GetGatewayList(ctx, &api.GetGatewayListRequest{
+	resp, err := gwClient.GetGatewayList(ctx, &m2mServer.GetGatewayListRequest{
 		OrgId:  req.OrgId,
 		Offset: req.Offset,
 		Limit:  req.Limit,
 	})
 	if err != nil {
+		log.WithError(err).Error(logInfo)
 		return &api.GetGatewayListResponse{}, status.Errorf(codes.Unavailable, err.Error())
 	}
 
+	var gatewayProfileList []*api.GSGatewayProfile
+	for _, item := range resp.GwProfile {
+		gatewayProfile := &api.GSGatewayProfile{
+			Id:          item.Id,
+			Mac:         item.Mac,
+			FkGwNs:      item.FkGwNs,
+			FkWallet:    item.FkWallet,
+			Mode:        api.GatewayMode(item.Mode),
+			CreateAt:    item.CreateAt,
+			LastSeenAt:  item.LastSeenAt,
+			OrgId:       item.OrgId,
+			Description: item.Description,
+			Name:        item.Name,
+		}
+
+		gatewayProfileList = append(gatewayProfileList, gatewayProfile)
+	}
+
 	return &api.GetGatewayListResponse{
-		GwProfile:   resp.GwProfile,
-		Count:       resp.Count,
-		UserProfile: &prof,
-	}, nil
+		GwProfile: gatewayProfileList,
+		Count:     resp.Count,
+	}, status.Error(codes.OK, "")
 }
 
 // GetGatewayProfile defines the get Gateway Profile request and response
 func (s *GatewayServerAPI) GetGatewayProfile(ctx context.Context, req *api.GetGSGatewayProfileRequest) (*api.GetGSGatewayProfileResponse, error) {
-	log.WithField("orgId", req.OrgId).Info("grpc_api/GetGatewayProfile")
+	logInfo := "api/appserver_serves_ui/GetGatewayProfile org=" + strconv.FormatInt(req.OrgId, 10)
 
-	prof, err := getUserProfileByJwt(ctx, s.validator, req.OrgId)
+	// verify if user is global admin
+	userIsAdmin, err := s.validator.GetIsAdmin(ctx)
 	if err != nil {
-		return &api.GetGSGatewayProfileResponse{}, status.Errorf(codes.Unauthenticated, err.Error())
+		log.WithError(err).Error(logInfo)
+		return &api.GetGSGatewayProfileResponse{}, status.Errorf(codes.Internal, "unable to verify user: %s", err.Error())
+	}
+	// is user is not global admin, user must have accesss to this organization
+	if userIsAdmin == false {
+		if err := s.validator.Validate(ctx, auth.ValidateOrganizationAccess(auth.Read, req.OrgId)); err != nil {
+			log.WithError(err).Error(logInfo)
+			return &api.GetGSGatewayProfileResponse{}, status.Errorf(codes.Unauthenticated, "authentication failed: %s", err.Error())
+		}
 	}
 
 	m2mClient, err := m2m_client.GetPool().Get(config.C.M2MServer.M2MServer, []byte(config.C.M2MServer.CACert),
 		[]byte(config.C.M2MServer.TLSCert), []byte(config.C.M2MServer.TLSKey))
 	if err != nil {
+		log.WithError(err).Error(logInfo)
 		return &api.GetGSGatewayProfileResponse{}, status.Errorf(codes.Unavailable, err.Error())
 	}
 
-	gwClient := api.NewGSGatewayServiceClient(m2mClient)
+	gwClient := m2mServer.NewGSGatewayServiceClient(m2mClient)
 
-	resp, err := gwClient.GetGatewayProfile(ctx, &api.GetGSGatewayProfileRequest{
+	resp, err := gwClient.GetGatewayProfile(ctx, &m2mServer.GetGSGatewayProfileRequest{
 		OrgId:  req.OrgId,
 		GwId:   req.GwId,
 		Offset: req.Offset,
 		Limit:  req.Limit,
 	})
 	if err != nil {
+		log.WithError(err).Error(logInfo)
 		return &api.GetGSGatewayProfileResponse{}, status.Errorf(codes.Unavailable, err.Error())
 	}
 
 	return &api.GetGSGatewayProfileResponse{
-		GwProfile:   resp.GwProfile,
-		UserProfile: &prof,
-	}, nil
+		GwProfile: &api.GSGatewayProfile{
+			Id:          resp.GwProfile.Id,
+			Mac:         resp.GwProfile.Mac,
+			FkGwNs:      resp.GwProfile.FkGwNs,
+			FkWallet:    resp.GwProfile.FkWallet,
+			Mode:        api.GatewayMode(resp.GwProfile.Mode),
+			CreateAt:    resp.GwProfile.CreateAt,
+			LastSeenAt:  resp.GwProfile.LastSeenAt,
+			OrgId:       resp.GwProfile.OrgId,
+			Description: resp.GwProfile.Description,
+			Name:        resp.GwProfile.Name,
+		},
+	}, status.Error(codes.OK, "")
 }
 
 // GetGatewayHistory defines the get Gateway History request and response
 func (s *GatewayServerAPI) GetGatewayHistory(ctx context.Context, req *api.GetGatewayHistoryRequest) (*api.GetGatewayHistoryResponse, error) {
-	log.WithField("orgId", req.OrgId).Info("grpc_api/GetGatewayHistory")
+	logInfo := "api/appserver_serves_ui/GetGatewayHistory org=" + strconv.FormatInt(req.OrgId, 10)
 
-	prof, err := getUserProfileByJwt(ctx, s.validator, req.OrgId)
+	// verify if user is global admin
+	userIsAdmin, err := s.validator.GetIsAdmin(ctx)
 	if err != nil {
-		return &api.GetGatewayHistoryResponse{}, status.Errorf(codes.Unauthenticated, err.Error())
+		log.WithError(err).Error(logInfo)
+		return &api.GetGatewayHistoryResponse{}, status.Errorf(codes.Internal, "unable to verify user: %s", err.Error())
+	}
+	// is user is not global admin, user must have accesss to this organization
+	if userIsAdmin == false {
+		if err := s.validator.Validate(ctx, auth.ValidateOrganizationAccess(auth.Read, req.OrgId)); err != nil {
+			log.WithError(err).Error(logInfo)
+			return &api.GetGatewayHistoryResponse{}, status.Errorf(codes.Unauthenticated, "authentication failed: %s", err.Error())
+		}
 	}
 
 	m2mClient, err := m2m_client.GetPool().Get(config.C.M2MServer.M2MServer, []byte(config.C.M2MServer.CACert),
 		[]byte(config.C.M2MServer.TLSCert), []byte(config.C.M2MServer.TLSKey))
 	if err != nil {
+		log.WithError(err).Error(logInfo)
 		return &api.GetGatewayHistoryResponse{}, status.Errorf(codes.Unavailable, err.Error())
 	}
 
-	gwClient := api.NewGSGatewayServiceClient(m2mClient)
+	gwClient := m2mServer.NewGSGatewayServiceClient(m2mClient)
 
-	resp, err := gwClient.GetGatewayHistory(ctx, &api.GetGatewayHistoryRequest{
+	resp, err := gwClient.GetGatewayHistory(ctx, &m2mServer.GetGatewayHistoryRequest{
 		OrgId:  req.OrgId,
 		GwId:   req.GwId,
 		Offset: req.Offset,
 		Limit:  req.Limit,
 	})
 	if err != nil {
+		log.WithError(err).Error(logInfo)
 		return &api.GetGatewayHistoryResponse{}, status.Errorf(codes.Unavailable, err.Error())
 	}
 
 	return &api.GetGatewayHistoryResponse{
 		GwHistory:   resp.GwHistory,
-		UserProfile: &prof,
-	}, nil
+	}, status.Error(codes.OK, "")
 }
 
 // SetGatewayMode defines the set Gateway mode request and response
 func (s *GatewayServerAPI) SetGatewayMode(ctx context.Context, req *api.SetGatewayModeRequest) (*api.SetGatewayModeResponse, error) {
-	log.WithField("orgId", req.OrgId).Info("grpc_api/SetGatewayMode")
+	logInfo := "api/appserver_serves_ui/SetGatewayMode org=" + strconv.FormatInt(req.OrgId, 10)
 
-	prof, err := getUserProfileByJwt(ctx, s.validator, req.OrgId)
+	// verify if user is global admin
+	userIsAdmin, err := s.validator.GetIsAdmin(ctx)
 	if err != nil {
-		return &api.SetGatewayModeResponse{}, status.Errorf(codes.Unauthenticated, err.Error())
+		log.WithError(err).Error(logInfo)
+		return &api.SetGatewayModeResponse{}, status.Errorf(codes.Internal, "unable to verify user: %s", err.Error())
 	}
+	// is user is not global admin, user must have accesss to this organization
+	if userIsAdmin == false {
+		if err := s.validator.Validate(ctx, auth.ValidateOrganizationAccess(auth.Read, req.OrgId)); err != nil {
+			log.WithError(err).Error(logInfo)
+			return &api.SetGatewayModeResponse{}, status.Errorf(codes.Unauthenticated, "authentication failed: %s", err.Error())
+		}
+	} else {
+		log.WithError(err).Error(logInfo)
+		return &api.SetGatewayModeResponse{}, status.Errorf(codes.Unauthenticated, "authentication failed: %s", err.Error())
+	}
+
 
 	m2mClient, err := m2m_client.GetPool().Get(config.C.M2MServer.M2MServer, []byte(config.C.M2MServer.CACert),
 		[]byte(config.C.M2MServer.TLSCert), []byte(config.C.M2MServer.TLSKey))
 	if err != nil {
+		log.WithError(err).Error(logInfo)
 		return &api.SetGatewayModeResponse{}, status.Errorf(codes.Unavailable, err.Error())
 	}
 
-	gwClient := api.NewGSGatewayServiceClient(m2mClient)
+	gwClient := m2mServer.NewGSGatewayServiceClient(m2mClient)
 
-	resp, err := gwClient.SetGatewayMode(ctx, &api.SetGatewayModeRequest{
+	resp, err := gwClient.SetGatewayMode(ctx, &m2mServer.SetGatewayModeRequest{
 		OrgId:  req.OrgId,
 		GwId:   req.GwId,
-		GwMode: req.GwMode,
+		GwMode: m2mServer.GatewayMode(req.GwMode),
 	})
 	if err != nil {
+		log.WithError(err).Error(logInfo)
 		return &api.SetGatewayModeResponse{}, status.Errorf(codes.Unavailable, err.Error())
 	}
 
 	return &api.SetGatewayModeResponse{
 		Status:      resp.Status,
-		UserProfile: &prof,
-	}, nil
+	}, status.Error(codes.OK, "")
 }
