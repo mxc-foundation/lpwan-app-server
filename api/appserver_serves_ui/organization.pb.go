b6f6613b3b01e614cfeb6fd6f49337335f8d1adb
Shiun Chen <123657@gmail.com>
2020-04-14 12:34:47 +0200

    add getuseremail api

--- api/appserver_serves_ui/organization.pb.go
+++ api/appserver_serves_ui/organization.pb.go
@@ -1166,11 +1166,11 @@ var fileDescriptor_8d10c68ef159b9ed = []byte{
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ context.Context
-var _ grpc.ClientConnInterface
+var _ grpc.ClientConn
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
-const _ = grpc.SupportPackageIsVersion6
+const _ = grpc.SupportPackageIsVersion4
 
 // OrganizationServiceClient is the client API for OrganizationService service.
 //
@@ -1199,10 +1199,10 @@ type OrganizationServiceClient interface {
 }
 
 type organizationServiceClient struct {
-	cc grpc.ClientConnInterface
+	cc *grpc.ClientConn
 }
 
-func NewOrganizationServiceClient(cc grpc.ClientConnInterface) OrganizationServiceClient {
+func NewOrganizationServiceClient(cc *grpc.ClientConn) OrganizationServiceClient {
 	return &organizationServiceClient{cc}
 }
 
