2a1d245afb0eb3d197c1278c27b0e7aa3a3f7ae2
lixuan <lixuan@mxc.org>
2020-06-27 00:47:45 +0200

    add mining to modules

--- internal/modules/mining/mining_controller.go
+++ internal/modules/mining/mining_controller.go
@@ -22,38 +22,6 @@ import (
 	"github.com/mxc-foundation/lpwan-app-server/internal/storage"
 )
 
-type CMC struct {
-	Status *Status `json:"status"`
-	Data   *Data   `json:"data"`
-}
-
-type Status struct {
-	Timestamp    string `json:"timestamp"`
-	ErrorCode    int    `json:"error_code"`
-	ErrorMessage string `json:"error_message"`
-	Elapsed      int    `json:"elapsed"`
-	CreditCount  int    `json:"credit_count"`
-	Notice       string `json:"notice"`
-}
-
-type Data struct {
-	Id          int    `json:"id"`
-	Symbol      string `json:"symbol"`
-	Name        string `json:"name"`
-	Amount      int    `json:"amount"`
-	LastUpdated string `json:"last_updated"`
-	Quote       struct {
-		MXC struct {
-			Price      float64 `json:"price"`
-			LastUpdate string  `json:"last_update"`
-		}
-		USD struct {
-			Price      float64 `json:"price"`
-			LastUpdate string  `json:"last_update"`
-		}
-	}
-}
-
 var dailyMxcPrice, totalMiningValue float64
 var rnd *rand.Rand
 
