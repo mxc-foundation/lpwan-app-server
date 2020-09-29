32583d246bbc69d2e507323de126cd28a2a5da7b
lixuan <lixuan@mxc.org>
2020-09-29 10:46:36 +0000

    Lixuan/md 706/new app server ui free

--- scripts/docker/docker-compose-template.py
+++ scripts/docker/docker-compose-template.py
@@ -2,49 +2,8 @@ import sys
 
 localDevelopTemplate = \
     '''
-    version: "2"
+    version: "3"
     services:  
-      network-server:
-        build:
-          context: ../lpwan-server
-          dockerfile: Dockerfile
-        volumes:
-          - ../lpwan-server/configuration:/etc/loraserver
-          - ../lpwan-server:/network-server
-        tty: true
-          
-      mxprotocol-server:
-        build:
-          context: ../mxprotocol-server
-          dockerfile: Dockerfile-devel
-        volumes:
-          - ../mxprotocol-server/configuration:/etc/mxprotocol-server
-          - ../mxprotocol-server:/mxprotocol-server
-        environment:
-          - APPSERVER=http://localhost:8080
-          - MXPROTOCOL_SERVER=http://localhost:4000
-        tty: true
-        ports:
-          - 4000:4000
-        security_opt:
-          - seccomp:unconfined
-        cap_add:
-          - SYS_PTRACE
-      
-      gatewaybridge:
-        image: mxcdocker/chirpstack-gateway-bridge
-        ports:
-          - 1700:1700/udp
-        volumes:
-          - ./configuration/chirpstack-gateway-bridge:/etc/chirpstack-gateway-bridge
-        restart: always
-    
-      geoserver:
-        image: chirpstack/chirpstack-geolocation-server:3
-        volumes:
-          - ./configuration/chirpstack-geolocation-server:/etc/chirpstack-geolocation-server
-        restart: always 
-        
       appserver:
         build:
           context: .
@@ -70,7 +29,48 @@ localDevelopTemplate = \
         cap_add:
           - SYS_PTRACE
         tty: true
+        
+      network-server:
+        build:
+          context: ../../lpwan-server
+          dockerfile: Dockerfile
+        volumes:
+          - ../../lpwan-server/configuration:/etc/loraserver
+          - ../../lpwan-server:/network-server
+        tty: true
+          
+      mxprotocol-server:
+        build:
+          context: ../../mxprotocol-server
+          dockerfile: Dockerfile-devel
+        volumes:
+          - ../../mxprotocol-server/configuration:/etc/mxprotocol-server
+          - ../../mxprotocol-server:/mxprotocol-server
+        environment:
+          - APPSERVER=http://localhost:8080
+          - MXPROTOCOL_SERVER=http://localhost:4000
+        tty: true
+        ports:
+          - 4000:4000
+        security_opt:
+          - seccomp:unconfined
+        cap_add:
+          - SYS_PTRACE
+      
+      gatewaybridge:
+        image: mxcdocker/chirpstack-gateway-bridge
+        ports:
+          - 1700:1700/udp
+        volumes:
+          - ./configuration/chirpstack-gateway-bridge:/etc/chirpstack-gateway-bridge
+        restart: always
     
+      geoserver:
+        image: chirpstack/chirpstack-geolocation-server:3
+        volumes:
+          - ./configuration/chirpstack-geolocation-server:/etc/chirpstack-geolocation-server
+        restart: always 
+
       postgresql:
         image: postgres:9.6-alpine
         volumes:
@@ -108,140 +108,8 @@ localDevelopTemplate = \
       #    - zookeeper
     '''
 
-remoteDevelopTemplate = \
-    '''
-    version: "2"
-    services:
-      appserver:
-        build:
-          context: .
-          dockerfile: Dockerfile-devel
-        volumes:
-          - ./configuration:/etc/lora-app-server
-          - ./:/lora-app-server  
-        links:
-          - redis
-        ports:
-          - 8080:8080
-          - 8004:8004
-          - 8005:8005
-        environment:
-          - SUPERNODE_DATA_SERVICE=remote
-        env_file:
-          - {}
-        security_opt:
-          - seccomp:unconfined
-        cap_add:
-          - SYS_PTRACE
-        tty: true
-        
-      redis:
-        image: redis:5-alpine
-    '''
-
-localTestingTemplate = \
-    '''
-    version: "3"
-    
-    services:
-      network-server:
-        image: mxcdocker/supernode:network-server.2.0.0-6-g956f51f
-        ports:
-          - 8000:8000
-        volumes:
-          - ./configuration/loraserver:/etc/loraserver
-    
-      appserver:
-        image: registry.gitlab.com/mxcfoundation/cloud/lpwan-app-server:latest
-        volumes:
-          - ./configuration/lora-app-server:/etc/lora-app-server
-        environment:
-          - SUPERNODE_DATA_SERVICE=local
-        env_file:
-          - {}
-        ports:
-          - 8080:8080
-          - 8004:8004
-          - 8005:8005
-        depends_on:
-          - postgresql
-          - redis
-          - mosquitto
-    
-      gatewaybridge:
-        image: loraserver/lora-gateway-bridge:3
-        ports:
-          - 1700:1700/udp
-        volumes:
-          - ./configuration/lora-gateway-bridge:/etc/lora-gateway-bridge
-    
-      geoserver:
-        image: loraserver/lora-geo-server:3
-        volumes:
-          - ./configuration/lora-geo-server:/etc/lora-geo-server
-    
-      postgresql:
-        image: postgres:9.6-alpine
-        env_file:
-          - {}
-        volumes:
-          - ./configuration/postgresql/initdb:/docker-entrypoint-initdb.d
-          - postgresqldata:/var/lib/postgresql/data
-        environment:
-          - POSTGRES_PASSWORD=local_superuser_pass
-    
-      redis:
-        image: redis:5-alpine
-        volumes:
-          - redisdata:/data
-    
-      mosquitto:
-        image: eclipse-mosquitto
-        ports:
-          - 1883:1883
-    
-      mxprotocol-server:
-        image: mxcdocker/mxprotocol-server:0.3.0-14-ga8be3482
-        volumes:
-          - ./configuration/mxprotocol-server:/etc/mxprotocol-server
-        environment:
-          - SUPERNODE_DATA_SERVICE=local
-        env_file:
-          - {}
-        depends_on:
-          - postgresql
-          - redis
-          - mosquitto
-        ports:
-          - 4000:4000
-    
-    volumes:
-      postgresqldata:
-      redisdata:
-    '''
-
-imageTemplate = \
-    '''
-    version: "2"
-    services:
-      appserver:
-        build:
-          context: .
-          dockerfile: Dockerfile
-        volumes:
-          - ./configuration:/etc/lora-app-server
-        ports:
-          - 8080:8080
-          - 8004:8004
-          - 8005:8005
-        environment:
-          - APPSERVER=http://localhost:8080
-          - MXPROTOCOL_SERVER=http://localhost:4000
-    '''
-
 inputList = {
     "env_file": "default",
-    "supernode_data_service": "default",
     "supernode_mode": "default",
 }
 
@@ -254,22 +122,9 @@ if __name__ == "__main__":
         key, value = item.split('=')
         inputList[key] = value
 
-    if (inputList["supernode_mode"] == "development") and (inputList["supernode_data_service"] == "local"):
+    if (inputList["supernode_mode"] == "development"):
         print(localDevelopTemplate.format(inputList["env_file"]))
         exit()
 
-    elif (inputList["supernode_mode"] == "development") and (inputList["supernode_data_service"] == "remote"):
-        print(remoteDevelopTemplate.format(inputList["env_file"]))
-        exit()
-
-    elif (inputList["supernode_mode"] == "testing") and (inputList["supernode_data_service"] == "local"):
-        print(localTestingTemplate.format(inputList["env_file"], inputList["env_file"], inputList["env_file"]))
-        exit()
-
-    elif (inputList["supernode_mode"] == "image") and (inputList["supernode_data_service"] == "image"):
-        print(imageTemplate)
-        exit()
-
     else:
-        print(inputList["supernode_mode"], inputList["supernode_data_service"])
         print("invalid argument")
