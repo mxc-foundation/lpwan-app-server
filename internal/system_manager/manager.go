10c1e90759d6d460a318835e9936c10f85de7bf1
Pavel Shaydo <pavel@mxc.org>
2020-10-26 15:18:51 +0100

    [MD-1706] initialize integrations before downlink
    if downlink is initialized before integrations then mqtt downChan is
    still nil and HandleDataDownPayloads goroutine blocks reading from the
    nil channel. On the other hand, when mqtt has been initialized and
    received any subscription data, txPayloadHandler callback blocks trying
    to write into mqtt's dataDownChan from which nobody reading.

--- internal/system_manager/manager.go
+++ internal/system_manager/manager.go
@@ -53,7 +53,7 @@ func SetupSystemModules() error {
 		return err
 	}
 
-	criticalStartUpList := []string{"migrations", "mxp_portal", "network_server", "provisioning_server_portal"}
+	criticalStartUpList := []string{"migrations", "mxp_portal", "network_server", "provisioning_server_portal", "integration"}
 	for _, v := range criticalStartUpList {
 		if _, ok := moduleSetupCallbackFunc[v]; !ok {
 			return fmt.Errorf("setup function is not found for %s", v)
