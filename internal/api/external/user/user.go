2bdf8fc3d8bc01b586cc109d3730bc0075fa2c27
lixuan <lixuan@mxc.org>
2020-11-09 11:55:39 +0100

    fix golint issue

--- internal/api/external/user/user.go
+++ internal/api/external/user/user.go
@@ -206,7 +206,7 @@ func (a *Server) Update(ctx context.Context, req *inpb.UpdateUserRequest) (*empt
 	if err != nil {
 		return nil, status.Errorf(codes.Unknown, "%v", err)
 	}
-	
+
 	user.IsActive = req.User.IsActive
 	user.SessionTTL = req.User.SessionTtl
 	user.Email = req.User.Username
