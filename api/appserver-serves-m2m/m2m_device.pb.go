c3e18beb5f2cc97354ebb939b7bca15112451dc4
lixuan <lixuan@mxc.org>
2020-11-26 17:39:47 +0100

    fix lint issue

--- api/appserver-serves-m2m/m2m_device.pb.go
+++ api/appserver-serves-m2m/m2m_device.pb.go
@@ -8,6 +8,9 @@ package appserver_serves_m2m
 
 import (
 	context "context"
+	reflect "reflect"
+	sync "sync"
+
 	proto "github.com/golang/protobuf/proto"
 	empty "github.com/golang/protobuf/ptypes/empty"
 	timestamp "github.com/golang/protobuf/ptypes/timestamp"
@@ -16,8 +19,6 @@ import (
 	status "google.golang.org/grpc/status"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
-	reflect "reflect"
-	sync "sync"
 )
 
 const (
