ed73a705d5b4efe1545bd71f0468eaf770e089ab
Pavel <pavel@mxc.org>
2021-04-16 18:01:26 +0200

    MMD-889 add an API call to get dhx mining history

--- api/appserver-serves-ui/dhx.pb.gw.go
+++ api/appserver-serves-ui/dhx.pb.gw.go
@@ -191,6 +191,42 @@ func local_request_DHXServcie_DHXGetLastMining_0(ctx context.Context, marshaler 
 
 }
 
+var (
+	filter_DHXServcie_DHXMiningHistory_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
+func request_DHXServcie_DHXMiningHistory_0(ctx context.Context, marshaler runtime.Marshaler, client DHXServcieClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq DHXMiningHistoryRequest
+	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_DHXServcie_DHXMiningHistory_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.DHXMiningHistory(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_DHXServcie_DHXMiningHistory_0(ctx context.Context, marshaler runtime.Marshaler, server DHXServcieServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq DHXMiningHistoryRequest
+	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_DHXServcie_DHXMiningHistory_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.DHXMiningHistory(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 func request_DHXServcie_DHXBond_0(ctx context.Context, marshaler runtime.Marshaler, client DHXServcieClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq DHXBondRequest
 	var metadata runtime.ServerMetadata
@@ -414,6 +450,29 @@ func RegisterDHXServcieHandlerServer(ctx context.Context, mux *runtime.ServeMux,
 
 	})
 
+	mux.Handle("GET", pattern_DHXServcie_DHXMiningHistory_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_DHXServcie_DHXMiningHistory_0(rctx, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_DHXServcie_DHXMiningHistory_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("POST", pattern_DHXServcie_DHXBond_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -624,6 +683,26 @@ func RegisterDHXServcieHandlerClient(ctx context.Context, mux *runtime.ServeMux,
 
 	})
 
+	mux.Handle("GET", pattern_DHXServcie_DHXMiningHistory_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_DHXServcie_DHXMiningHistory_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_DHXServcie_DHXMiningHistory_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("POST", pattern_DHXServcie_DHXBond_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -698,6 +777,8 @@ var (
 
 	pattern_DHXServcie_DHXGetLastMining_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"api", "dhx-mining", "last-mining"}, "", runtime.AssumeColonVerbOpt(true)))
 
+	pattern_DHXServcie_DHXMiningHistory_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"api", "dhx-mining", "history"}, "", runtime.AssumeColonVerbOpt(true)))
+
 	pattern_DHXServcie_DHXBond_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"api", "dhx-mining", "bond"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_DHXServcie_DHXUnbond_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"api", "dhx-mining", "unbond"}, "", runtime.AssumeColonVerbOpt(true)))
@@ -715,6 +796,8 @@ var (
 	forward_DHXServcie_DHXListStakes_0 = runtime.ForwardResponseMessage
 
 	forward_DHXServcie_DHXGetLastMining_0 = runtime.ForwardResponseMessage
+
+	forward_DHXServcie_DHXMiningHistory_0 = runtime.ForwardResponseMessage
 
 	forward_DHXServcie_DHXBond_0 = runtime.ForwardResponseMessage
 
