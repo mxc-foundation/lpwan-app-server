d4bb464bf389f1fbdde3e6cb0a7c64a9827e468c
lixuan <lixuan@mxc.org>
2021-06-10 16:16:30 +0200

    divide stream response to smaller pieces for testing

--- internal/api/external/report/mining_report.go
+++ internal/api/external/report/mining_report.go
@@ -143,7 +143,7 @@ func (s *Server) MiningReportPDF(req *api.MiningReportRequest, srv api.ReportSer
 
 func sendStream(data bytes.Buffer, send func(response *api.MiningReportResponse) error) (err error) {
 	for {
-		rb := make([]byte, 1000)
+		rb := make([]byte, 100)
 		n, err := io.ReadFull(&data, rb)
 		if err != nil {
 			if err == io.ErrUnexpectedEOF {
